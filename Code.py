4ffc836d3568f801b53f7d946aaf0bcd395dec18
Vincent Graillat <vincent.graillat@hec.edu>
2023-11-14 13:04:58 +0100

    Scrapping classement

--- Code.py
+++ Code.py
@@ -1,4 +1,18 @@
-import pandas as pd 
-print ("hello")
-print("yo")
-print("zebi")
+import pandas as pd
+from bs4 import BeautifulSoup
+import requests
+url = "https://fr.wikipedia.org/wiki/Championnat_de_France_de_football_2022-2023"
+page = requests.get(url)
+soup = BeautifulSoup(page.text, "html.parser")
+classement = soup.find("table", class_="wikitable sortable center")
+equipes =[]
+balises_a = classement.find_all('a')
+for balise in balises_a[1:]:
+    equipes.append(balise.text)
+
+data = {'équipes': equipes}
+for i in range(1, 39):
+    nom_colonne = 'J{}'.format(i)
+    data[nom_colonne] = [''] * len(equipes)
+df = pd.DataFrame(data)
+print(df)
