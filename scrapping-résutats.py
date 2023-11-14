ce2d372e7faec6b6ac47e0026a411e0f11ac8345
Vincent Graillat <vincent.graillat@hec.edu>
2023-11-14 20:58:43 +0100

    début scrapping résultats

--- scrapping-résutats.py
+++ scrapping-résutats.py
@@ -4,9 +4,12 @@ from bs4 import BeautifulSoup
 import requests
 
 #on cherche à scrapper les données de classement à chaque journée de la saison 2022/2023
-url = "https://fr.wikipedia.org/wiki/Championnat_de_France_de_football_2022-2023"
+url = "https://www.footballcritic.com/ligue-1/season-2022-2023/matches/4/65453"
 page = requests.get(url)
 soup = BeautifulSoup(page.text, "html.parser")
 
 #on isole le tableau qui nous intéresse
-classement = soup.find("table", class_="wikitable")
+résultats = soup.find("span", class_="rounds r_67146 w_38")
+colonnes = ["Journée", "Domicile", "Extérieur", "Buts domicile", "Buts extérieur", "Résultat"]
+résultats_22_23 = pd.DataFrame(columns=colonnes)
+
