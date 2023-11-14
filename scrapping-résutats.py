b99c105e510c283ffee8101edd7c0de587725fa3
Vincent Graillat <vincent.graillat@hec.edu>
2023-11-14 23:24:29 +0100

    Finalisation J38 22_23

--- scrapping-résutats.py
+++ scrapping-résutats.py
@@ -10,6 +10,33 @@ soup = BeautifulSoup(page.text, "html.parser")
 
 #on isole le tableau qui nous intéresse
 résultats = soup.find("span", class_="rounds r_67146 w_38")
-colonnes = ["Journée", "Domicile", "Extérieur", "Buts domicile", "Buts extérieur", "Résultat"]
-résultats_22_23 = pd.DataFrame(columns=colonnes)
 
+equipes = résultats.select('.info-box .text.hidden-xs a')
+equipes_final =[]
+for element in equipes:
+    equipes_final.append(element.text.strip())
+
+equipes_domicile = equipes_final[::2]
+equipes_exterieure = equipes_final[1::2]
+
+scores = résultats.find_all('span', class_='score-text')
+scores_int =[]
+for element in scores:
+    element2 = element.text.strip()
+    scores_int.append(element2.split(' - '))
+    scores_final = [item for sublist in scores_int for item in sublist]
+
+scores_domicile = scores_final[::2]
+scores_exterieure = scores_final[1::2]
+
+
+résultats_22_23 = pd.DataFrame()
+résultats_22_23['Journée'] = [1]*10
+résultats_22_23['Domicile'] = equipes_domicile
+résultats_22_23['Extérieur'] = equipes_exterieure
+résultats_22_23['Buts domicile'] = scores_domicile
+résultats_22_23['Buts extérieur'] = scores_exterieure
+résultats_22_23['Résultat'] = 'E'
+résultats_22_23.loc[résultats_22_23['Buts domicile'] > résultats_22_23['Buts extérieur'], 'Résultat'] = 'D'
+résultats_22_23.loc[résultats_22_23['Buts domicile'] == résultats_22_23['Buts extérieur'], 'Résultat'] = 'N'
+print(résultats_22_23)
