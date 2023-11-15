d507e508c808c80c9663e031fe6b27688aaccdca
Vincent Graillat <vincent.graillat@hec.edu>
2023-11-15 12:20:58 +0100

    fonction dataframe résultats

--- scrapping-résutats.py
+++ scrapping-résutats.py
@@ -2,121 +2,94 @@
 import pandas as pd
 from bs4 import BeautifulSoup
 import requests
+import re
+
+#on créé une fonction qui permettent de scrapper les résultats de chaque journée, en prenant en entrée un url et un nom de classe HTML
+def fonction_résultats(url,classe):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, "html.parser")
+    noms_colonnes = ['Saison','Journée','Domicile','Extérieur','Buts domicile','Buts extérieur','Résultat']
+    résultats_finaux = pd.DataFrame(columns=noms_colonnes)
+    for i in range(1,39):
+        tableau = soup.find_all("span", class_="rounds r_" + str(classe) + " w_" + str(i))
+        equipes = []
+        for element in tableau:
+            equipes.append(element.select('.info-box .text.hidden-xs a'))
+        equipes_finales =[]
+        for equipe in equipes:
+            for balise_a in equipe:
+                texte_equipe = BeautifulSoup(str(balise_a), 'html.parser').get_text()
+                equipes_finales.append(texte_equipe)
+        equipes_finales = [equipe.strip() for equipe in equipes_finales]
+
+        equipes_domicile = equipes_finales[::2]
+        equipes_exterieure = equipes_finales[1::2]
+
+        scores = []
+        for element in tableau:
+            scores.append(element.find_all('span', class_='score-text'))
+        scores_int =[]
+        for element in scores:
+            scores_matche = re.findall(r'\d+\s*-\s*\d+', str(element))
+            scores_int.extend(scores_matche)
+        scores_int = [element.split(' - ') for element in scores_int]
+        scores_finaux = [item for sublist in scores_int for item in sublist]
 
 
-#on cherche à scrapper les données de classement à chaque journée de la saison 2022/2023
-url = "https://www.footballcritic.com/ligue-1/season-2022-2023/matches/4/65453"
-page = requests.get(url)
-soup = BeautifulSoup(page.text, "html.parser")
-noms_colonnes = ['Journée','Domicile','Extérieur','Buts domicile','Buts extérieur','Résultat']
-résultats_22_23_finaux = pd.DataFrame(columns=noms_colonnes)
-#on effectue notre requête sur les 38 journées de Ligue 1
-#on remarque qu'il y a un soucis à la journée 2. On divise donc en trois parties 
-#la J1
-for i in range(1,2):
-    #on isole le tableau qui nous intéresse
-    résultats = soup.find("span", class_="rounds r_67146 w_" + str(i))
+        scores_domicile = scores_finaux[::2]
+        scores_exterieure = scores_finaux[1::2]
 
-    #on construit la liste des équipes jouant à domicile, et celle des équipes jouant à l'extérieur correspondante
-    equipes = résultats.select('.info-box .text.hidden-xs a')
-    equipes_final =[]
-    for element in equipes:
-        equipes_final.append(element.text.strip())
+        résultats = pd.DataFrame(columns=noms_colonnes)
+        url_parts = url.split("/")
+        annee_part = [part for part in url_parts if "season" in part][0]
+        annee = annee_part.split("-")[1]
+        résultats['Saison']=annee*10
+        résultats['Journée']= [i]*10
+        résultats['Domicile'] = equipes_domicile
+        résultats['Extérieur'] = equipes_exterieure
+        résultats['Buts domicile'] = scores_domicile
+        résultats['Buts extérieur'] = scores_exterieure
+        résultats['Résultat'] = 'E'
+        résultats.loc[résultats['Buts domicile'] > résultats['Buts extérieur'], 'Résultat'] = 'D'
+        résultats.loc[résultats['Buts domicile'] == résultats['Buts extérieur'], 'Résultat'] = 'N'
+        résultats_finaux = résultats_finaux._append(résultats, ignore_index=True)
 
-    equipes_domicile = equipes_final[::2]
-    equipes_exterieure = equipes_final[1::2]
+    return résultats_finaux
 
-    #on construit la liste des buts marqués à domicile, et celle des buts marqués à l'extérieur correspondante
-    scores = résultats.find_all('span', class_='score-text')
-    scores_int =[]
-    for element in scores:
-        element2 = element.text.strip()
-        scores_int.append(element2.split(' - '))
-        scores_final = [item for sublist in scores_int for item in sublist]
 
-    scores_domicile = scores_final[::2]
-    scores_exterieure = scores_final[1::2]
+#on crée la liste des url et des noms de classes correspondants pour les saisons de Ligue 1 depuis 2010
+liste = [
+    ["https://www.footballcritic.com/ligue-1/season-2022-2023/matches/4/65453",67146],
+    ["https://www.footballcritic.com/ligue-1/season-2021-2022/matches/4/51604",55330],
+    ["https://www.footballcritic.com/ligue-1/season-2020-2021/matches/4/35278",46313],
+    ["https://www.footballcritic.com/ligue-1/season-2019-2020/matches/4/21586",32460],
+    ["https://www.footballcritic.com/ligue-1/season-2018-2019/matches/4/16810",22368],
+    ["https://www.footballcritic.com/ligue-1/season-2017-2018/matches/4/13295",12876],
+    ["https://www.footballcritic.com/ligue-1/season-2016-2017/matches/4/11798",8078],
+    ["https://www.footballcritic.com/ligue-1/season-2015-2016/matches/4/7793",5880],
+    ["https://www.footballcritic.com/ligue-1/season-2014-2015/matches/4/3817",3643],
+    ["https://www.footballcritic.com/ligue-1/season-2013-2014/matches/4/2730",2751],
+    ["https://www.footballcritic.com/ligue-1/season-2012-2013/matches/4/166",170],
+    ["https://www.footballcritic.com/ligue-1/season-2011-2012/matches/4/167",171],
+    ["https://www.footballcritic.com/ligue-1/season-2010-2011/matches/4/168",172],
+    ["https://www.footballcritic.com/ligue-1/season-2009-2010/matches/4/169",173],
+    ["https://www.footballcritic.com/ligue-1/season-2008-2009/matches/4/170",174],
+    ["https://www.footballcritic.com/ligue-1/season-2007-2008/matches/4/171",175],
+    ["https://www.footballcritic.com/ligue-1/season-2006-2007/matches/4/172",176],
+    ["https://www.footballcritic.com/ligue-1/season-2005-2006/matches/4/173",177],
+    ["https://www.footballcritic.com/ligue-1/season-2004-2005/matches/4/174",178],
+    ["https://www.footballcritic.com/ligue-1/season-2003-2004/matches/4/175",179],
+    ["https://www.footballcritic.com/ligue-1/season-2002-2003/matches/4/176",180],
+    ["https://www.footballcritic.com/ligue-1/season-2001-2002/matches/4/177",181],
+    ["https://www.footballcritic.com/ligue-1/season-2000-2001/matches/4/178",182]
+]
 
-    #on crée un data frame avec les résultats
-    noms_colonnes = ['Journée','Domicile','Extérieur','Buts domicile','Buts extérieur','Résultat']
-    résultats_22_23 = pd.DataFrame(columns=noms_colonnes)
-    résultats_22_23['Journée']= [i]*10
-    résultats_22_23['Domicile'] = equipes_domicile
-    résultats_22_23['Extérieur'] = equipes_exterieure
-    résultats_22_23['Buts domicile'] = scores_domicile
-    résultats_22_23['Buts extérieur'] = scores_exterieure
-    résultats_22_23['Résultat'] = 'E'
-    résultats_22_23.loc[résultats_22_23['Buts domicile'] > résultats_22_23['Buts extérieur'], 'Résultat'] = 'D'
-    résultats_22_23.loc[résultats_22_23['Buts domicile'] == résultats_22_23['Buts extérieur'], 'Résultat'] = 'N'
-    résultats_22_23_finaux = résultats_22_23_finaux._append(résultats_22_23, ignore_index=True)
+#on créé le data frame final en parcourant les éléments de la liste et en utilisant notre fonction 
+noms_colonnes = ['Saison','Journée','Domicile','Extérieur','Buts domicile','Buts extérieur','Résultat']
+dataframe_résultats = pd.DataFrame(columns=noms_colonnes)
+for element in liste:
+    résultats_finaux = fonction_résultats(element[0],element[1])
+    dataframe_résultats = dataframe_résultats._append(résultats_finaux, ignore_index=True)
 
-#la J2
-for i in range(2,3):
-    résultats = soup.find("span", class_="rounds r_67146 w_" + str(i))
+print(dataframe_résultats)
 
-    equipes = résultats.select('.info-box .text.hidden-xs a')
-    equipes_final =[]
-    for element in equipes:
-        equipes_final.append(element.text.strip())
-
-    equipes_domicile = equipes_final[::2]
-    equipes_exterieure = equipes_final[1::2]
-
-    scores = résultats.find_all('span', class_='score-text')
-    scores_int =[]
-    for element in scores:
-        element2 = element.text.strip()
-        scores_int.append(element2.split(' - '))
-        scores_final = [item for sublist in scores_int for item in sublist]
-
-    scores_domicile = scores_final[::2]
-    scores_exterieure = scores_final[1::2]
-
-    noms_colonnes = ['Journée','Domicile','Extérieur','Buts domicile','Buts extérieur','Résultat']
-    résultats_22_23 = pd.DataFrame(columns=noms_colonnes)
-    résultats_22_23['Journée']= [i]*9
-    résultats_22_23['Domicile'] = equipes_domicile
-    résultats_22_23['Extérieur'] = equipes_exterieure
-    résultats_22_23['Buts domicile'] = scores_domicile
-    résultats_22_23['Buts extérieur'] = scores_exterieure
-    résultats_22_23['Résultat'] = 'E'
-    résultats_22_23.loc[résultats_22_23['Buts domicile'] > résultats_22_23['Buts extérieur'], 'Résultat'] = 'D'
-    résultats_22_23.loc[résultats_22_23['Buts domicile'] == résultats_22_23['Buts extérieur'], 'Résultat'] = 'N'
-    #on rajoute le résultat qu'il nous manquait
-    manquant = {'Journée':'2','Domicile' : 'Lorient', 'Extérieur':'Lyon','Buts domicile':'3','Buts extérieur':'1','Résultat':'D'}
-    résultats_22_23 = résultats_22_23._append(manquant, ignore_index=True)
-    résultats_22_23_finaux = résultats_22_23_finaux._append(résultats_22_23, ignore_index=True)
-
-for i in range(3,39):
-    résultats = soup.find("span", class_="rounds r_67146 w_" + str(i))
-
-    equipes = résultats.select('.info-box .text.hidden-xs a')
-    equipes_final =[]
-    for element in equipes:
-        equipes_final.append(element.text.strip())
-
-    equipes_domicile = equipes_final[::2]
-    equipes_exterieure = equipes_final[1::2]
-
-    scores = résultats.find_all('span', class_='score-text')
-    scores_int =[]
-    for element in scores:
-        element2 = element.text.strip()
-        scores_int.append(element2.split(' - '))
-        scores_final = [item for sublist in scores_int for item in sublist]
-
-    scores_domicile = scores_final[::2]
-    scores_exterieure = scores_final[1::2]
-
-    noms_colonnes = ['Journée','Domicile','Extérieur','Buts domicile','Buts extérieur','Résultat']
-    résultats_22_23 = pd.DataFrame(columns=noms_colonnes)
-    résultats_22_23['Journée']= [i]*10
-    résultats_22_23['Domicile'] = equipes_domicile
-    résultats_22_23['Extérieur'] = equipes_exterieure
-    résultats_22_23['Buts domicile'] = scores_domicile
-    résultats_22_23['Buts extérieur'] = scores_exterieure
-    résultats_22_23['Résultat'] = 'E'
-    résultats_22_23.loc[résultats_22_23['Buts domicile'] > résultats_22_23['Buts extérieur'], 'Résultat'] = 'D'
-    résultats_22_23.loc[résultats_22_23['Buts domicile'] == résultats_22_23['Buts extérieur'], 'Résultat'] = 'N'
-    résultats_22_23_finaux = résultats_22_23_finaux._append(résultats_22_23, ignore_index=True)
-print(résultats_22_23_finaux)
