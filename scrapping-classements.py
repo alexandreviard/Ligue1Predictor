a68d2f0fa21ba75c0ffe0c2e2cc78849b3ffd987
Vincent Graillat <vincent.graillat@hec.edu>
2023-11-15 21:11:23 +0100

    Fin classement depuis 2009-2010

--- scrapping-classements.py
+++ scrapping-classements.py
@@ -2,343 +2,80 @@
 import pandas as pd
 from bs4 import BeautifulSoup
 import requests
-
-#on cherche à scrapper les données de classement à chaque journée de la saison 2022/2023
-url = "https://fr.wikipedia.org/wiki/Championnat_de_France_de_football_2022-2023"
-page = requests.get(url)
-soup = BeautifulSoup(page.text, "html.parser")
-
-#on isole le tableau qui nous intéresse
-classement = soup.find("table", class_="wikitable sortable center")
-equipes =[]
-balises_a = classement.find_all('a')
-for balise in balises_a[1:]:
-    equipes.append(balise.text)
-
-#on crée un dataframe#
-colonnes = ["Équipes"] + ["J{}".format(i) for i in range(1, 39)]
-classement_22_23 = pd.DataFrame(columns=colonnes)
-
-#on y ajoute les classements pour chaque journée
-x = classement.find_all('td')
-for i in range(20):
-    liste = [x.get_text(strip=True) for x in x[1+41*i:39+41*i]]
-    liste = [equipes[i]] + liste
-    classement_22_23.loc[len(classement_22_23)] = liste
-
-#on fait la même chose pour la saison 2021/2022
-url = "https://fr.wikipedia.org/wiki/Championnat_de_France_de_football_2021-2022"
-page = requests.get(url)
-soup = BeautifulSoup(page.text, "html.parser")
-
-classement = soup.find("table", class_="wikitable sortable center")
-equipes =[]
-balises_a = classement.find_all('a')
-for balise in balises_a[5:]:
-    equipes.append(balise.text)
-
-colonnes = ["Équipes"] + ["J{}".format(i) for i in range(1, 39)]
-classement_21_22 = pd.DataFrame(columns=colonnes)
-
-x = classement.find_all('td')
-#il y a une coquille dans le script html de wiki (manque une colonne sur la ligne de l'OGC Nice) donc on découpe le code en deux parties
-for i in range(5):
-    liste = [x.get_text(strip=True) for x in x[1+42*i:39+42*i]]
-    liste = [equipes[i]] + liste
-    classement_21_22.loc[len(classement_21_22)] = liste
-for i in range(5,20):
-    liste = [x.get_text(strip=True) for x in x[0+42*i:38+42*i]]
-    liste = [equipes[i]] + liste
-    classement_21_22.loc[len(classement_21_22)] = liste
-
-
-#on fait la même chose pour la saison 2020/2021
-url = "https://fr.wikipedia.org/wiki/Championnat_de_France_de_football_2020-2021"
-page = requests.get(url)
-soup = BeautifulSoup(page.text, "html.parser")
-
-classement = soup.find("table", class_="wikitable sortable center")
-equipes =[]
-balises_a = classement.find_all('a')
-for balise in balises_a[7:]:
-    equipes.append(balise.text)
-
-colonnes = ["Équipes"] + ["J{}".format(i) for i in range(1, 39)]
-classement_20_21 = pd.DataFrame(columns=colonnes)
-
-x = classement.find_all('td')
-for i in range(20):
-    liste = [x.get_text(strip=True) for x in x[1+42*i:39+42*i]]
-    liste = [equipes[i]] + liste
-    classement_20_21.loc[len(classement_20_21)] = liste
-
-
-#on fait la même chose pour la saison 2019/2020. Année COVID oblige, certains petits changements s'imposent.
-url = "https://fr.wikipedia.org/wiki/Championnat_de_France_de_football_2019-2020"
-page = requests.get(url)
-soup = BeautifulSoup(page.text, "html.parser")
-
-classement = soup.find("table", class_="wikitable sortable center")
-equipes =[]
-balises_a = classement.find_all('a')
-for balise in balises_a[6:]:
-    equipes.append(balise.text)
-
-colonnes = ["Équipes"] + ["J{}".format(i) for i in range(1, 29)]
-classement_19_20 = pd.DataFrame(columns=colonnes)
-
-x = classement.find_all('td')
-for i in range(20):
-    liste = [x.get_text(strip=True) for x in x[1+32*i:29+32*i]]
-    liste = [equipes[i]] + liste
-    classement_19_20.loc[len(classement_19_20)] = liste
-
-#on fait la même chose pour la saison 2018/2019
-url = "https://fr.wikipedia.org/wiki/Championnat_de_France_de_football_2018-2019"
-page = requests.get(url)
-soup = BeautifulSoup(page.text, "html.parser")
-
-classement = soup.find("table", class_="wikitable sortable center")
-equipes =[]
-balises_a = classement.find_all('a')
-for balise in balises_a[7:]:
-    equipes.append(balise.text)
-
-colonnes = ["Équipes"] + ["J{}".format(i) for i in range(1, 39)]
-classement_18_19 = pd.DataFrame(columns=colonnes)
-
-x = classement.find_all('td')
-for i in range(20):
-    liste = [x.get_text(strip=True) for x in x[1+42*i:39+42*i]]
-    liste = [equipes[i]] + liste
-    classement_18_19.loc[len(classement_18_19)] = liste
-
-#on fait la même chose pour la saison 2017/2018
-url = "https://fr.wikipedia.org/wiki/Championnat_de_France_de_football_2017-2018"
-page = requests.get(url)
-soup = BeautifulSoup(page.text, "html.parser")
-
-classement = soup.find("table", class_="wikitable sortable center")
-equipes =[]
-balises_a = classement.find_all('a')
-for balise in balises_a[4:]:
-    equipes.append(balise.text)
-
-colonnes = ["Équipes"] + ["J{}".format(i) for i in range(1, 39)]
-classement_17_18 = pd.DataFrame(columns=colonnes)
-
-x = classement.find_all('td')
-for i in range(20):
-    liste = [x.get_text(strip=True) for x in x[1+42*i:39+42*i]]
-    liste = [equipes[i]] + liste
-    classement_17_18.loc[len(classement_17_18)] = liste
-
-#on fait la même chose pour la saison 2016/2017
-url = "https://fr.wikipedia.org/wiki/Championnat_de_France_de_football_2016-2017"
-page = requests.get(url)
-soup = BeautifulSoup(page.text, "html.parser")
-
-classement = soup.find("table", class_="wikitable sortable center")
-equipes =[]
-balises_a = classement.find_all('a')
-for balise in balises_a[8:]:
-    equipes.append(balise.text)
-
-colonnes = ["Équipes"] + ["J{}".format(i) for i in range(1, 39)]
-classement_16_17 = pd.DataFrame(columns=colonnes)
-
-x = classement.find_all('td')
-for i in range(20):
-    liste = [x.get_text(strip=True) for x in x[1+42*i:39+42*i]]
-    liste = [equipes[i]] + liste
-    classement_16_17.loc[len(classement_16_17)] = liste
-
-#on fait la même chose pour la saison 2015/2016.
-url = "https://fr.wikipedia.org/wiki/Championnat_de_France_de_football_2015-2016"
-page = requests.get(url)
-soup = BeautifulSoup(page.text, "html.parser")
-
-#Le nom de la classe ici n'est pas le même que précédemment
-classement = soup.find("table", class_="wikitable sortable center nowrap")
-equipes =[]
-balises_a = classement.find_all('a')
-for balise in balises_a[0:]:
-    equipes.append(balise.text)
-
-colonnes = ["Équipes"] + ["J{}".format(i) for i in range(1, 39)]
-classement_15_16 = pd.DataFrame(columns=colonnes)
-
-x = classement.find_all('td')
-for i in range(20):
-    liste = [x.get_text(strip=True) for x in x[1+41*i:39+41*i]]
-    liste = [equipes[i]] + liste
-    classement_15_16.loc[len(classement_15_16)] = liste
-
-#Ici le classement n'est pas en ordre croissant, on rajoute donc un peu de code
-classement_15_16["J38"] = classement_15_16["J38"].astype(int)
-classement_15_16 = classement_15_16.sort_values(by="J38")
-classement_15_16 = classement_15_16.reset_index(drop=True)
-classement_15_16["J38"] = classement_15_16["J38"].astype(str)
-
-#on fait la même chose pour la saison 2014/2015
-url = "https://fr.wikipedia.org/wiki/Championnat_de_France_de_football_2014-2015"
-page = requests.get(url)
-soup = BeautifulSoup(page.text, "html.parser")
-
-classement = soup.find("table", class_="wikitable sortable center nowrap")
-equipes =[]
-balises_a = classement.find_all('a')
-for balise in balises_a[0:]:
-    equipes.append(balise.text)
-
-colonnes = ["Équipes"] + ["J{}".format(i) for i in range(1, 39)]
-classement_14_15 = pd.DataFrame(columns=colonnes)
-
-x = classement.find_all('td')
-for i in range(20):
-    liste = [x.get_text(strip=True) for x in x[1+41*i:39+41*i]]
-    liste = [equipes[i]] + liste
-    classement_14_15.loc[len(classement_14_15)] = liste
-
-classement_14_15["J38"] = classement_14_15["J38"].astype(int)
-classement_14_15 = classement_14_15.sort_values(by="J38")
-classement_14_15 = classement_14_15.reset_index(drop=True)
-classement_14_15["J38"] = classement_14_15["J38"].astype(str)
-
-
-#on fait la même chose pour la saison 2013/2014
-url = "https://fr.wikipedia.org/wiki/Championnat_de_France_de_football_2013-2014"
-page = requests.get(url)
-soup = BeautifulSoup(page.text, "html.parser")
-
-classement = soup.find("table", class_="wikitable sortable center")
-equipes =[]
-balises_a = classement.find_all('a')
-for balise in balises_a[0:]:
-    equipes.append(balise.text)
-
-colonnes = ["Équipes"] + ["J{}".format(i) for i in range(1, 39)]
-classement_13_14 = pd.DataFrame(columns=colonnes)
-
-x = classement.find_all('td')
-for i in range(20):
-    liste = [x.get_text(strip=True) for x in x[1+41*i:39+41*i]]
-    liste = [equipes[i]] + liste
-    classement_13_14.loc[len(classement_13_14)] = liste
-
-classement_13_14["J38"] = classement_13_14["J38"].astype(int)
-classement_13_14 = classement_13_14.sort_values(by="J38")
-classement_13_14 = classement_13_14.reset_index(drop=True)
-classement_13_14["J38"] = classement_13_14["J38"].astype(str)
-
-#on fait la même chose pour la saison 2012/2013
-url = "https://fr.wikipedia.org/wiki/Championnat_de_France_de_football_2012-2013"
-page = requests.get(url)
-soup = BeautifulSoup(page.text, "html.parser")
-
-classement = soup.find("table", class_="wikitable sortable center")
-equipes =[]
-balises_a = classement.find_all('a')
-for balise in balises_a[0:]:
-    equipes.append(balise.text)
-
-colonnes = ["Équipes"] + ["J{}".format(i) for i in range(1, 39)]
-classement_12_13 = pd.DataFrame(columns=colonnes)
-
-x = classement.find_all('td')
-for i in range(20):
-    liste = [x.get_text(strip=True) for x in x[1+41*i:39+41*i]]
-    liste = [equipes[i]] + liste
-    classement_12_13.loc[len(classement_12_13)] = liste
-
-classement_12_13["J38"] = classement_12_13["J38"].astype(int)
-classement_12_13 = classement_12_13.sort_values(by="J38")
-classement_12_13 = classement_12_13.reset_index(drop=True)
-classement_12_13["J38"] = classement_12_13["J38"].astype(str)
-
-#on fait la même chose pour la saison 2011/2012
-url = "https://fr.wikipedia.org/wiki/Championnat_de_France_de_football_2011-2012"
-page = requests.get(url)
-soup = BeautifulSoup(page.text, "html.parser")
-
-classement = soup.find("table", class_="wikitable sortable center")
-equipes =[]
-balises_a = classement.find_all('a')
-for balise in balises_a[0:]:
-    equipes.append(balise.text)
-
-colonnes = ["Équipes"] + ["J{}".format(i) for i in range(1, 39)]
-classement_11_12 = pd.DataFrame(columns=colonnes)
-
-x = classement.find_all('td')
-for i in range(20):
-    liste = [x.get_text(strip=True) for x in x[1+41*i:39+41*i]]
-    liste = [equipes[i]] + liste
-    classement_11_12.loc[len(classement_11_12)] = liste
-
-classement_11_12["J38"] = classement_11_12["J38"].astype(int)
-classement_11_12 = classement_11_12.sort_values(by="J38")
-classement_11_12 = classement_11_12.reset_index(drop=True)
-classement_11_12["J38"] = classement_11_12["J38"].astype(str)
-
-#A partir de maintenant les noms des équipes sont des acronymes/sigles, il faut donc créer un glossaire
-acronymes_clubs = {
-    'PSG': 'Paris',
-    'OM': 'Marseille',
-    'OL': 'Lyon',
-    'ASSE': 'Saint-Étienne',
-    'LOSC': 'Lille',
-    'ASM': 'Monaco',
-    'FCN': 'Nantes',
-    'OGCN': 'Nice',
-    'RCSA': 'Strasbourg',
-    'SRFC': 'Rennes',
-    'FCSM': 'Sochaux',
-    'FCGB': 'Bordeaux',
-    'TFC': 'Toulouse',
-    'AJA': 'Auxerre',
-    'FCL': 'Lorient',
-    'VAFC': 'Valenciennes',
-    'ASNL': 'Nancy',
-    'MHSC': 'Montpellier',
-    'SMC': 'Caen',
-    'SB29': 'Brest',
-    'RCL': 'Lens',
-    'ACAA': 'Arles-Avignon',
-    'ETGFC':'Évian TG',
-    'ACA': 'Ajaccio',
-    'DFCO':'Dijon'
-}
-
-#on change ensuite le nom des équipes 
-classement_11_12['Équipes'] = classement_11_12['Équipes'].map(acronymes_clubs)
-
-print(classement_11_12)
-
-
-#on fait la même chose pour la saison 2010/2011
-url = "https://fr.wikipedia.org/wiki/Championnat_de_France_de_football_2010-2011"
-page = requests.get(url)
-soup = BeautifulSoup(page.text, "html.parser")
-
-classement = soup.find("table", class_="wikitable sortable center")
-equipes =[]
-balises_a = classement.find_all('a')
-for balise in balises_a[0:]:
-    equipes.append(balise.text)
-
-colonnes = ["Équipes"] + ["J{}".format(i) for i in range(1, 39)]
-classement_10_11 = pd.DataFrame(columns=colonnes)
-
-x = classement.find_all('td')
-for i in range(20):
-    liste = [x.get_text(strip=True) for x in x[1+39*i:39+39*i]]
-    liste = [equipes[i]] + liste
-    classement_10_11.loc[len(classement_10_11)] = liste
-
-classement_10_11["J38"] = classement_10_11["J38"].astype(int)
-classement_10_11 = classement_10_11.sort_values(by="J38")
-classement_10_11 = classement_10_11.reset_index(drop=True)
-classement_10_11["J38"] = classement_10_11["J38"].astype(str)
-classement_10_11['Équipes'] = classement_10_11['Équipes'].map(acronymes_clubs)
-print(classement_10_11)
+import re
+
+def fonction_classement(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, "html.parser")
+
+    annee = re.search(r'\d{4}-\d{4}', url).group()
+    if annee == "2009-2010":
+        supplement = " centre"
+    else:
+        supplement = " center"
+    if annee == "2015-2016" or annee=="2014-2015":
+        supplement2 = " nowrap"
+    else:
+        supplement2 = ""
+
+    
+    tableau = soup.find("table", class_="wikitable sortable" + str(supplement) + str(supplement2))
+    n = tableau.get_text().count("[r ") + tableau.get_text().count("[a ") + tableau.get_text().count("[e ")
+    print(n)
+    balises_td = tableau.find_all('td')
+    m = len(balises_td)
+    equipes =[]
+    balises_a = tableau.find_all('a')
+    for balise in balises_a[n:]:
+        equipes.append(balise.text)
+    
+    #en raison du Covid, le nombre de journée n'est pas le même en 2019-2020
+    if annee == "2019-2020":
+        y=29
+    else:
+        y=39
+
+    #on crée un dataframe#
+    colonnes = ["Équipes"] + ["J{}".format(i) for i in range(1, y)]
+    classement = pd.DataFrame(columns=colonnes)
+
+    #on y ajoute les classements pour chaque journée
+    if annee == "2021-2022":
+        for i in range(5):
+            liste = [x.get_text(strip=True) for x in balises_td[1+42*i:39+42*i]]
+            liste = [equipes[i]] + liste
+            classement.loc[len(classement)] = liste
+        for i in range(5,20):
+            liste = [x.get_text(strip=True) for x in balises_td[0+42*i:38+42*i]]
+            liste = [equipes[i]] + liste
+            classement.loc[len(classement)] = liste
+    else: 
+        for i in range(20):
+            liste = [x.get_text(strip=True) for x in balises_td[1+int(m/20)*i:y+int(m/20)*i]]
+            liste = [equipes[i]] + liste
+            classement.loc[len(classement)] = liste
+    
+    if annee == "2019-2020":
+        for i in range(29,39):
+            nom_colonne = "J" + str(i)
+            classement[nom_colonne] = 0
+    return classement
+
+liste_url = []
+
+for annee in range(2009, 2023):
+    lien = f"https://fr.wikipedia.org/wiki/Championnat_de_France_de_football_{annee}-{annee+1}"
+    liste_url.append(lien)
+
+noms_colonnes = ["Équipes"] + ["J{}".format(i) for i in range(1, 39)]
+dataframe_classement = pd.DataFrame(columns=noms_colonnes)
+#on ajoute les résultats pour chaque saison, en vérifiant qu'il n'y a pas d'erreur
+for element in liste_url:
+    try:
+        classement = fonction_classement(element)
+        dataframe_classement = dataframe_classement._append(classement, ignore_index=True)
+    except Exception as e:
+        print(f"Erreur à l'élément {element}: {e}")
+
+
+    
--- scrapping-résutats.py
+++ scrapping-résutats.py
@@ -4,25 +4,34 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
-#on créé une fonction qui permettent de scrapper les résultats de chaque journée, en prenant en entrée un url et un nom de classe HTML
-def fonction_résultats(url,classe):
+#on crée une fonction qui permettent de scrapper les résultats de chaque journée, en prenant en entrée un url et un nom de classe HTML
+#on définit la fonction
+def fonction_resultats(url,classe):
+    #on récupère le script HTML grace à BeautifulSoup
     page = requests.get(url)
     soup = BeautifulSoup(page.text, "html.parser")
+    #on crée un data frame
     noms_colonnes = ['Saison','Journée','Domicile','Extérieur','Buts domicile','Buts extérieur','Résultat']
-    résultats_finaux = pd.DataFrame(columns=noms_colonnes)
+    resultats_finaux = pd.DataFrame(columns=noms_colonnes)
+    #on isole dans le l'url la saison dont il est question
     url_parts = url.split("/")
     annee_part = [part for part in url_parts if "season" in part][0]
     annee = annee_part.replace("season-", "")
+    #en raison du Covid, le nombre de journée n'est pas le même en 2019-2020
     if annee == "2019-2020":
         x=29
     else:
         x=39
     
+    #on crée une boucle pour avoir chacune des 38 journées
     for i in range(1,x):
+        #on isole dans la page le tableau contenant les résultats
         tableau = soup.find_all("span", class_="rounds r_" + str(classe) + " w_" + str(i))
+        #on récupère la liste des équipes
         equipes = []
         for element in tableau:
             equipes.append(element.select('.info-box .text.hidden-xs a'))
+        #on met au propre la liste des équipes
         equipes_finales =[]
         for equipe in equipes:
             for balise_a in equipe:
@@ -30,39 +39,50 @@ def fonction_résultats(url,classe):
                 equipes_finales.append(texte_equipe)
         equipes_finales = [equipe.strip() for equipe in equipes_finales]
 
+        #dans notre liste on a [équipe, adversaire, équipe, adversaire ...]
+        #on peut donc récupérer la liste des équipes jouant à domicile et celle des équipes jouant à l'extérieur
         equipes_domicile = equipes_finales[::2]
-        equipes_exterieure = equipes_finales[1::2]
+        equipes_exterieur = equipes_finales[1::2]
 
+        #on récupère la liste des scores
         scores = []
         for element in tableau:
             scores.append(element.select('.btn-info'))
+        #on met au propre la liste des scores
         scores_int =[]
         for element in scores:
-            scores_matche = re.findall(r'>-<|\d+\s*-\s*\d+|>20:45<|>21:00<|>17:00<',str(element))
-            scores_int.extend(scores_matche) 
+            #en raison de certains matchs qui ne se sont pas joué (Strasbourg-PSG 2019-2020) ou où le résultat a été changé après le match (Nantes-Bastia 2013-2014), il faut être vigilant 
+            #dans ces cas là au lieu du score, il y a soit l'heure du match soit un tiret
+            #dans les années étudiées, on trouve 4 de ces occurences
+            scores_match = re.findall(r'>-<|\d+\s*-\s*\d+|>20:45<|>21:00<|>17:00<',str(element))
+            scores_int.extend(scores_match) 
+        #on décide de mettre 0-0 pour les 4 matches en question 
         scores_int = ['0 - 0' if item in ('>-<','>20:45<','>21:00<','>17:00<') else item for item in scores_int]  
+        #on met au propre la liste des scores
         scores_int = [element.split(' - ') for element in scores_int]
         scores_finaux = [item for sublist in scores_int for item in sublist]
-        
+
+        #on récupère la liste des scores des équipes jouant à domicile et celle des scores des équipes jouant à l'extérieur   
         scores_domicile = scores_finaux[::2]
-        scores_exterieure = scores_finaux[1::2]
+        scores_exterieur = scores_finaux[1::2]
 
-        résultats = pd.DataFrame(columns=noms_colonnes)
-        url_parts = url.split("/")
-        annee_part = [part for part in url_parts if "season" in part][0]
-        annee = annee_part.replace("season-", "")
-        résultats['Saison']=[annee]*10
-        résultats['Journée']= [i]*10
-        résultats['Domicile'] = equipes_domicile
-        résultats['Extérieur'] = equipes_exterieure
-        résultats['Buts domicile'] = scores_domicile
-        résultats['Buts extérieur'] = scores_exterieure
-        résultats['Résultat'] = 'E'
-        résultats.loc[résultats['Buts domicile'] > résultats['Buts extérieur'], 'Résultat'] = 'D'
-        résultats.loc[résultats['Buts domicile'] == résultats['Buts extérieur'], 'Résultat'] = 'N'
-        résultats_finaux = résultats_finaux._append(résultats, ignore_index=True)
-
-    return résultats_finaux
+        #on peut maintenant compléter notre dataframe
+        resultats = pd.DataFrame(columns=noms_colonnes)
+        resultats['Saison']=[annee]*10
+        resultats['Journée']= [i]*10
+        resultats['Domicile'] = equipes_domicile
+        resultats['Extérieur'] = equipes_exterieur
+        resultats['Buts domicile'] = scores_domicile
+        resultats['Buts extérieur'] = scores_exterieur
+        #on met le résultat du match ("D" pour victoire de Domicile, "N" pour Nul et "E" pour victoire d'Extérieur)
+        resultats['Résultat'] = 'E'
+        resultats.loc[resultats['Buts domicile'] > resultats['Buts extérieur'], 'Résultat'] = 'D'
+        resultats.loc[resultats['Buts domicile'] == resultats['Buts extérieur'], 'Résultat'] = 'N'
+        #on ajoute les données pour chaques journées
+        resultats_finaux = resultats_finaux._append(resultats, ignore_index=True)
+    
+    #on retourne le data frame final des résultats de la saison
+    return resultats_finaux
 
 
 #on crée la liste des url et des noms de classes correspondants pour les saisons de Ligue 1 depuis 2010
@@ -92,13 +112,15 @@ liste = [
 
 #on créé le data frame final en parcourant les éléments de la liste et en utilisant notre fonction 
 noms_colonnes = ['Saison','Journée','Domicile','Extérieur','Buts domicile','Buts extérieur','Résultat']
-dataframe_résultats = pd.DataFrame(columns=noms_colonnes)
+dataframe_resultats = pd.DataFrame(columns=noms_colonnes)
+#on ajoute les résultats pour chaque saison, en vérifiant qu'il n'y a pas d'erreur
 for element in liste:
     try:
-        résultats_finaux = fonction_résultats(element[0],element[1])
-        dataframe_résultats = dataframe_résultats._append(résultats_finaux, ignore_index=True)
+        resultats_finaux = fonction_resultats(element[0],element[1])
+        dataframe_resultats = dataframe_resultats._append(resultats_finaux, ignore_index=True)
     except Exception as e:
         print(f"Erreur à l'élément {element}: {e}")
-
-dataframe_résultats.to_csv('dataframe_résultats.csv', index=False)
+    
+#on enregistre le dataframe final au format csv
+dataframe_resultats.to_csv('dataframe_résultats.csv', index=False)
 
