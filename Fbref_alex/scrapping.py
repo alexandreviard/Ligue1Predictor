2201f1d610951fce0f736e4c659075b1d54d3ba7
alexandreviard <alexandre.viard@ensae.fr>
2023-12-24 20:26:27 +0000

    scrapping + automatisation scrapp

--- Fbref_alex/scrapping.py
+++ Fbref_alex/scrapping.py
@@ -5,134 +5,193 @@ import time
 
 def scrape_ligue1_data(nb_saisons=6):
     """
-    Cette fonction scrape les données de Ligue 1 pour un nombre donné de saisons.
+    Récupère les données de la Ligue 1 sur plusieurs saisons spécifiées.
+    
+    Args:
+        nb_saisons (int): Nombre de saisons à récupérer.
 
-    :param nb_saisons: Nombre de saisons de données à scraper.
-    :return: DataFrame contenant les données agrégées de chaque saison.
+    Returns:
+        DataFrame: Un DataFrame Pandas contenant les données agrégées.
     """
-    # URL de base pour les données de la Ligue 1
+    # Définition de l'URL de base et des en-têtes pour les requêtes
     url_base_ligue1 = "https://fbref.com/en/comps/13/Ligue-1-Stats"
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    all_seasons_data = []
 
-    # En-tête de l'utilisateur pour simuler une requête depuis un navigateur
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:97.0) Gecko/20100101 Firefox/97.0'
-    }
-
-
-    all_seasons_data = []  # Liste pour stocker les données de toutes les saisons
-
+    # Boucle sur le nombre de saisons demandé
     for saison in range(nb_saisons):
-        time.sleep(3)  # Applique le contrôle du taux de requêtes
+        # Application du contrôle de taux de requête
+        rate_limit()
 
-        # Scraping des URLs des équipes pour la saison courante
+        # Exécution de la requête et analyse HTML
         response = requests.get(url_base_ligue1, headers=headers)
         soup = BeautifulSoup(response.text, 'html.parser')
-        teams_urls = [f"https://fbref.com{a.get('href')}" for a in soup.select("table.stats_table a") if "squads" in a.get('href')]
+
+        # Récupération des URLs des équipes
+        teams_urls = ["https://fbref.com" + equipe.get("href") 
+                      for equipe in soup.select("table.stats_table")[0].find_all("a") 
+                      if "squads" in equipe.get("href", "")]
 
         # Mise à jour de l'URL pour la prochaine saison
         url_base_ligue1 = f"https://fbref.com{soup.find('a', class_='button2 prev').get('href')}"
 
+        # Traitement pour chaque équipe
         for team_url in teams_urls:
-            time.sleep(3)  # Applique le contrôle du taux de requêtes
+            # Application du contrôle de taux de requête
+            rate_limit()
 
-            # Scraping des données de l'équipe
+            # Récupération et préparation des données de l'équipe
             team_response = requests.get(team_url, headers=headers)
             team_data = pd.read_html(team_response.text, match="Scores")[0]
             team_name = team_url.split("/")[-1].replace("-Stats", "").replace("-", " ")
             team_data["Team"] = team_name
 
-            # Scraping des URLs des statistiques détaillées
-            soup_team = BeautifulSoup(team_response.text, 'html.parser')
-            stats_urls = [f"https://fbref.com{a.get('href')}" for a in soup_team.find_all("a") if "matchlogs/all_comps" in a.get('href', '') and any(x in a.get('href', '') for x in ["passing/", "shooting", "possession/", "defense/", "keeper"])]
+            # Récupération des URLs pour les statistiques détaillées
+            stats_urls = get_stats_urls(team_response)
 
+            # Traitement des statistiques détaillées
             for stats_url in set(stats_urls):
-                time.sleep(3) # Applique le contrôle du taux de requêtes
-
-                # Scraping des statistiques détaillées
-                stats_response = requests.get(stats_url, headers=headers)
-                detailed_stats = pd.read_html(stats_response.text)[0]
-                if detailed_stats.columns.nlevels > 1:
-                    detailed_stats.columns = [f"{col}_{branch}" if "For" not in col and "Unnamed:" not in col else f"{branch}" for col, branch in detailed_stats.columns]
-
-                columns_to_drop = ["Time", "Comp", "Round", "Day", "Venue", "Result", "GF", "GA", "Opponent"] + [col for col in detailed_stats.columns if 'Report' in col]
-                detailed_stats.drop(columns_to_drop, axis=1, inplace=True)
-
+                rate_limit()
+                detailed_stats = get_detailed_stats(stats_url, headers)
                 team_data = team_data.merge(detailed_stats, on="Date")
 
+            # Ajout des données de l'équipe au résultat global
             all_seasons_data.append(team_data)
 
-    # Concaténation des données de toutes les saisons
-    final_data = pd.concat(all_seasons_data, ignore_index=True)
-    return final_data
-
-# Exemple d'utilisation de la fonction
-df_ligue1 = scrape_ligue1_data(nb_saisons=6)
+    # Concaténation de toutes les données des saisons
+    return pd.concat(all_seasons_data, ignore_index=True)
 
 
-def scrap_ligue1_last():
+def scrape_latest_ligue1_data():
+    """
+    Récupère les dernières données disponibles pour chaque équipe de la Ligue 1.
+
+    Returns:
+        DataFrame: Un DataFrame Pandas avec les dernières données de chaque équipe.
+    """
+    # Configuration initiale similaire à scrape_ligue1_data()
     url_ligue1 = "https://fbref.com/en/comps/13/Ligue-1-Stats"
-left_on = ["Time", "Comp", "Round", "Day", "Venue", "Result", "GF", "GA", "Opponent"]
-df_final = []
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    latest_data = []
 
-# En-tête de l'utilisateur pour ressembler à Mozilla Firefox
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:97.0) Gecko/20100101 Firefox/97.0'
-}
+    response = requests.get(url_ligue1, headers=headers)
+    soup = BeautifulSoup(response.text, 'html.parser')
 
+    teams_urls = ["https://fbref.com" + equipe.get("href") 
+                  for equipe in soup.select("table.stats_table")[0].find_all("a") 
+                  if "squads" in equipe.get("href", "")]
 
-request1 = requests.get(url_ligue1, headers=headers)
-soup = BeautifulSoup(request1.text, 'html.parser')
-url_equipes = soup.select("table.stats_table")[0].find_all("a")
-url_equipes = [equipes.get("href") for equipes in url_equipes]
-url_equipes = [equipes for equipes in url_equipes if equipes and "squads" in equipes]
-url_equipes = [f"https://fbref.com{i}" for i in url_equipes]
+    # Traitement similaire à scrape_ligue1_data() pour chaque équipe
+    for team_url in teams_urls:
+        rate_limit()
+        team_response = requests.get(team_url, headers=headers)
+        team_data = pd.read_html(team_response.text, match="Scores")[0]
+        team_name = team_url.split("/")[-1].replace("-Stats", "").replace("-", " ")
+        team_data["Team"] = team_name
 
+        stats_urls = get_stats_urls(team_response)
 
+        for stats_url in set(stats_urls):
+            rate_limit()
+            detailed_stats = get_detailed_stats(stats_url, headers)
+            team_data = team_data.merge(detailed_stats, on="Date")
 
-for j in range(len(url_equipes)):
+        latest_data.append(team_data)
 
-    time.sleep(3)  # Applique le contrôle du taux de requêtes
-
-    request2 = requests.get(url_equipes[j], headers=headers)
-    soup2 = BeautifulSoup(request2.text, 'html.parser')
-    df_equipe = pd.read_html(request2.text, match="Scores")[0]
-
-    nom_equipe = url_equipes[j].split("/")[-1].replace("-Stats", "").replace("-", " ")
-    df_equipe["equipe"] = nom_equipe
-
-    url_stats = soup2.findAll("a")
-    url_stats = [el.get("href") for el in url_stats]
-    url_stats = [el for el in url_stats if el and "matchlogs/all_comps" in el]
-    url_stats = [el for el in url_stats if el and ("passing/" in el or "shooting" in el or "possession/" in el or "defense/" in el or "keeper" in el)]
-    url_stats = list(set(url_stats))
-    url_stats = [f"https://fbref.com{i}" for i in url_stats]
+    # Retourne les données concaténées de toutes les équipes
+    return pd.concat(latest_data, ignore_index=True)
 
 
-    for y in range(len(url_stats)):
-        time.sleep(3)  # Applique le contrôle du taux de requêtes
+def rate_limit():
+    """
+    Fonction pour limiter le taux de requêtes et éviter de surcharger le serveur.
+    Utilise un délai d'attente entre les requêtes pour respecter les limitations.
+    """
+    MIN_REQUEST_INTERVAL = 2.5
+    last_request_time = getattr(rate_limit, "last_request_time", None)
+    if last_request_time is not None:
+        elapsed_time = time.time() - last_request_time
+        if elapsed_time < MIN_REQUEST_INTERVAL:
+            time.sleep(MIN_REQUEST_INTERVAL - elapsed_time)
+    rate_limit.last_request_time = time.time()
 
-        request3 = requests.get(url_stats[y], headers=headers)
-        soup3 = BeautifulSoup(request3.text, 'html.parser')
-        stats = pd.read_html(request3.text)[0]
 
-        if stats.columns.nlevels > 1:
-            stats.columns = [f"{col}_{branch}" if "For" not in col and "Unnamed:" not in col else f"{branch}" for col, branch in stats.columns]
+def get_stats_urls(team_response):
+    """
+    Récupère les URLs pour les statistiques détaillées d'une équipe.
 
-        stats.drop(left_on + [col for col in stats.columns if 'Report' in col], axis=1, inplace=True)
+    Args:
+        team_response: Réponse de la requête de la page de l'équipe.
 
-        df_equipe = df_equipe.merge(stats, on="Date")
+    Returns:
+        Set[str]: Ensemble d'URLs des statistiques détaillées.
+    """
+    soup_team = BeautifulSoup(team_response.text, 'html.parser')
 
-    df_final.append(df_equipe)
+    # Extraction des URLs pour les statistiques détaillées
+    url_stats = {
+        f"https://fbref.com{a.get('href')}" 
+        for a in soup_team.find_all("a") 
+        if "matchlogs/all_comps" in a.get('href', '') and 
+           any(substring in a.get('href', '') for substring in ["passing/", "shooting", "possession/", "defense/", "keeper"])
+    }
+    return url_stats
 
-df_final2 = pd.concat(df_final)
+
+def get_detailed_stats(stats_url, headers):
+    """
+    Récupère les statistiques détaillées à partir de l'URL donnée.
+
+    Args:
+        stats_url: URL de la page des statistiques détaillées.
+        headers: En-têtes pour la requête.
+
+    Returns:
+        DataFrame: DataFrame des statistiques détaillées.
+    """
+    stats_response = requests.get(stats_url, headers=headers)
+    detailed_stats = pd.read_html(stats_response.text)[0]
+
+    # Nettoyage des colonnes du DataFrame
+    if detailed_stats.columns.nlevels > 1:
+        detailed_stats.columns = [f"{col}_{branch}" 
+                                  if "For" not in col and "Unnamed:" not in col 
+                                  else f"{branch}" 
+                                  for col, branch in detailed_stats.columns]
+    columns_to_drop = ["Time", "Comp", "Round", "Day", "Venue", "Result", "GF", "GA", "Opponent"] + [col for col in detailed_stats.columns if 'Report' in col]
+    detailed_stats.drop(columns_to_drop, axis=1, inplace=True)
+
+    return detailed_stats
 
 
 def add_new_matches(base_initiale, base_nouvelle):
+    """
+    Ajoute les nouvelles données à la base de données initiale en évitant les doublons.
 
-    # Le scrapping change l'odre des colonnes il faut donc les réaligner pour concaténer
-    base_nouvelle = base_nouvelle[base_initiale.columns]
+    Args:
+        base_initiale: DataFrame de la base de données initiale.
+        base_nouvelle: DataFrame des nouvelles données à ajouter.
 
-    # Concaténation des deux bases de données et suppression des doublons
-    concatenated_data = pd.concat([base_nouvelle, base_initiale]).drop_duplicates().sort_values(by ="DateTime").reset_index(drop=True)
+    Returns:
+        DataFrame: DataFrame mis à jour avec les nouvelles données.
+    """
 
-    return concatenated_data
+    # Suppression de la colonne "Unnamed: 0" si elle existe
+    if 'Unnamed: 0' in base_initiale.columns:
+        base_initiale.drop(columns=['Unnamed: 0'], inplace=True)
+    if 'Unnamed: 0' in base_nouvelle.columns:
+        base_nouvelle.drop(columns=['Unnamed: 0'], inplace=True)
+
+    # Concaténation de la base initiale et de la base nouvelle
+    concatenated_df = pd.concat([base_initiale, base_nouvelle])
+    
+    # Suppression des doublons basée sur les colonnes "Date", "Team" et "Opponent"
+    concatenated_df_dedup = concatenated_df.drop_duplicates(subset=['Date', 'Team', 'Opponent'])
+    
+    # Tri par "Date" et "Team"
+    concatenated_df_sorted = concatenated_df_dedup.sort_values(by=['Date', 'Team'])
+
+    # Réinitialisation de l'index
+    concatenated_df_final = concatenated_df_sorted.reset_index(drop=True)
+    
+    return concatenated_df_final
