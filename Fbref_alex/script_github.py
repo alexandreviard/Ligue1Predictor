6e8668c750222e375228e65244d1e171632ad55c
alexandreviard <151853129+alexandreviard@users.noreply.github.com>
2023-12-25 17:13:57 +0100

    Update script_github.py

--- Fbref_alex/script_github.py
+++ Fbref_alex/script_github.py
@@ -1,8 +1,16 @@
 from script_automatisation import *
 
+
+
 def update_data():
 
-    base_actuelle = pd.read_csv("Fbref_alex/SOCCER_241223.csv")
+    mapping_equipe = {
+        'Nimes': 'Nîmes',
+        'Paris S-G': 'Paris Saint Germain',
+        'Saint Etienne': 'Saint-Étienne'
+    }
+    
+    base_actuelle = pd.read_csv("Fbref_alex/SOCCER_241223.csv", index_col=0)
     data = scrape_latest_ligue1_data()
     base_supplémentaire = data[0]
 
@@ -12,8 +20,10 @@ def update_data():
     base_updated.to_csv("Fbref_alex/SOCCER_241223.csv")
     
     recup_matchweek = data[1]
-
+    
+    recup_matchweek=find_futur_matchweeks(recup_matchweek, mapping_equipe)
     #recup_matchweek.to_csv("/home/onyxia/work/Projet-python/Fbref_alex/recup_matchweek.csv")
     recup_matchweek.to_csv("Fbref_alex/recup_matchweek.csv")
 
     return
+update_data()
